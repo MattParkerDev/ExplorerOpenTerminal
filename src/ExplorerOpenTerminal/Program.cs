4dee718416fa5d1a38fc9ea8a1fd78c74891fcc9
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2025-02-15 19:04:54 +1000

    get active tab handle working

--- src/ExplorerOpenTerminal/Program.cs
+++ src/ExplorerOpenTerminal/Program.cs
@@ -4,6 +4,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Automation;
 using Vanara.PInvoke;
+using Vanara.Windows.Shell;
 
 class Program
 {
@@ -63,7 +64,9 @@ class Program
     static string GetActiveExplorerPath(IntPtr activeWindow)
     {
 	    var handleAndFolderPaths = GetExplorerWindows();
-	    return handleAndFolderPaths.First(s => s.Handle == activeWindow).FolderPath;
+	    var path = handleAndFolderPaths.First(s => s.Handle == activeWindow);
+	    GetActiveExplorerTab(path.Handle);
+	    return path.FolderPath;
     }
 
     private static List<HandleAndFolderPath> GetExplorerWindows()
@@ -75,19 +78,78 @@ class Program
 	    ArgumentNullException.ThrowIfNull(shellAppType);
 	    dynamic? shellApp = Activator.CreateInstance(shellAppType);
 	    ArgumentNullException.ThrowIfNull(shellApp);
+	    //Shell32.IShellWindows shellWindows = (Shell32.IShellWindows)shellApp;
+	    Vanara.Windows.Shell.ShellBrowser shellBrowser2 = new ShellBrowser();
+	    var test2 = shellBrowser2.Text;
+	    var items = shellBrowser2.Items;
+
+
+	    //Shell32.IShellWindows shellWindows2 = new ;
 
 	    // Get all open Explorer windows
 	    var windows = shellApp!.Windows();
 
-	    foreach (var window in windows)
+	    foreach (var window in windows) // Window is apparently an IWebBrowser2
 	    {
 		    if (window == null) continue;
 
 		    var handlerPointer = new IntPtr((int)window.HWND);
 		    var folderPath = window.Document.Folder.Self.Path as string;
+		    var test = window.LocationName;
+		    var test3 = window.LocationURL;
 
 		    handleAndFolderPaths.Add(new HandleAndFolderPath(handlerPointer, folderPath));
 	    }
 	    return handleAndFolderPaths;
+	}
+
+	static string? GetActiveExplorerTab(IntPtr hwnd)
+	{
+		// Try to retrieve the active tab
+		IntPtr activeTab = IntPtr.Zero;
+
+		var activeTabHandle = User32.FindWindowEx(hwnd, IntPtr.Zero, "ShellTabWindowClass");
+		var activeTabPointer = activeTabHandle.DangerousGetHandle();
+		if (activeTabPointer == IntPtr.Zero)
+			return null;
+
+		// Use Shell.Application to get the correct tab
+		var shellAppType = Type.GetTypeFromProgID("Shell.Application");
+		dynamic? shellApp = Activator.CreateInstance(shellAppType);
+		if (shellApp == null)
+			return null;
+
+		var windows = shellApp!.Windows();
+		foreach (var window in windows)
+		{
+			if (window == null || new IntPtr((int)window.HWND) != hwnd)
+				continue;
+
+			// Query IShellBrowser
+			var shellBrowserPtr = Marshal.GetIUnknownForObject(window);
+			Guid IID_IShellBrowser = new("000214E2-0000-0000-C000-000000000046");
+			IntPtr shellBrowser;
+			Marshal.QueryInterface(shellBrowserPtr, ref IID_IShellBrowser, out shellBrowser);
+
+
+			uint thisTab;
+			Marshal.ReadIntPtr(shellBrowser, 3 * IntPtr.Size); // Call VTable index 3 for active tab
+			DllCall(shellBrowser, out thisTab);
+
+			if (thisTab == (uint)activeTab)
+			{
+				return window.Document.Folder.Self.Path as string;
+			}
+		}
+		return null;
+	}
+
+	private static void DllCall(IntPtr shellBrowser, out uint thisTab)
+	{
+		IntPtr vTable = Marshal.ReadIntPtr(shellBrowser);
+		IntPtr getActiveTabPtr = Marshal.ReadIntPtr(vTable, 3 * IntPtr.Size);
+		//var getActiveTab = (delegate* unmanaged[Stdcall]<IntPtr, out uint, int>)getActiveTabPtr;
+		//getActiveTab(shellBrowser, out thisTab);
+		thisTab = 0;
 	}
 }
